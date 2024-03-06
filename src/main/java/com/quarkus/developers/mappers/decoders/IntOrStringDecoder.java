33519ed3018f64218948a8ebab9e57242dc685ff
danilo <d.cividin@hotmail.it>
2024-03-06 15:59:57 +0100

    fixed decoder

--- src/main/java/com/quarkus/developers/mappers/decoders/IntOrStringDecoder.java
+++ src/main/java/com/quarkus/developers/mappers/decoders/IntOrStringDecoder.java
@@ -6,6 +6,14 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class IntOrStringDecoder {
     public String asString(IntOrString intOrString) {
-        return intOrString == null ? null : intOrString.getStrVal();
+        return intOrString == null ? null : getIntOrString(intOrString);
+    }
+
+    String getIntOrString(IntOrString intOrString) {
+        if(intOrString.getStrVal() == null || intOrString.getStrVal().isEmpty()) {
+            return "" + intOrString.getIntVal();
+        } else {
+            return intOrString.getStrVal();
+        }
     }
 }
